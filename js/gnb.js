1fd7e63b031d27e2294a79677d54e5e045f929e9
danilobrinu <ddbn.c2@gmail.com>
2013-10-28 15:26:29 -0500

    ultimos cambios a gnb

--- js/gnb.js
+++ js/gnb.js
@@ -31,7 +31,7 @@ $(window).on('load', function() {
   /**
    * Initialize Ubigeo
    */
-  $.getJSON('js/ubigeo.json')
+  $.getJSON('js/ubigeoGNB.json')
     .done(function(data) {
       ubigeo = data;
       loadDepartments();
@@ -79,22 +79,11 @@ $(window).on('load', function() {
    * Initialize GMap
    */
   var GMap = function() {
-    if (navigator.geolocation) {
-      var getMyPosition = function(position) {
-        showMap({
-          latitude: position.coords.latitude, 
-          longitude: position.coords.longitude,
-          name: 'BIENVENIDO A BNG'
-        }, true);
-      };
-      var errorHandler = function() {
-        alert('Error al recoger los datos de su coordenada actual');
-      };
-      /* get my current coords (lat, lng) */
-      navigator.geolocation.getCurrentPosition(getMyPosition, errorHandler, {maximumAge: 24 * 60 * 60 * 1000});
-    } else {
-      alert('Su navegador no soporta geolocalización');
-    }
+    showMap({
+      latitude: -12.128605, 
+      longitude: -76.977698,
+      name: 'BIENVENIDO A BNG'
+    }, true);
   };
   GMap();
 });
@@ -115,7 +104,7 @@ $buttonSearch.on('click', function() {
   });
 });
 
-function showMap(serviceChannel, defaultMap) {
+function showMap(serviceChannel, defaultMarker) {
   google.maps.visualRefresh = true;
   serviceChannelCoordinates = new google.maps.LatLng(serviceChannel.latitude, serviceChannel.longitude);
   mapOptions = {
@@ -125,12 +114,12 @@ function showMap(serviceChannel, defaultMap) {
       mapTypeControl: false
   };
   map = new google.maps.Map(document.getElementById('googleMapGNB'), mapOptions);
-  if (!defaultMap) {
+  if (!defaultMarker) {
     icon = {
-      url: 'img/icon-bng.png',
-      size: new google.maps.Size(40, 32),
-      origin: new google.maps.Point(0,0),
-      anchor: new google.maps.Point(0, 32)
+      url: 'img/markerGNB.png',
+      size: new google.maps.Size(36, 50),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(0, 50)
     }
     marker = new google.maps.Marker({
         position: serviceChannelCoordinates,
@@ -145,7 +134,6 @@ function showMap(serviceChannel, defaultMap) {
         title: serviceChannel.name 
     });
   }
-  
 }
 
 function search() {
@@ -154,29 +142,28 @@ function search() {
       districtChosen = $districts.val(),
       addressChosen = $address.val().toUpperCase(),
       serviceChannelsResults = _.filter(serviceChannels, function(serviceChannel) {
-        var $serviceChannelTypeChecked = $('input[name=serviceChannelType]:checked');
+        var serviceChannelTypeCheckeds = $('input[name=serviceChannelType]:checked');
         return serviceChannel.department == departmentChosen && 
                serviceChannel.province == provinceChosen && 
                serviceChannel.district == districtChosen && 
                serviceChannel.address.search(addressChosen) != -1 &&
-               ( $serviceChannelTypeChecked.length == 2 ? (serviceChannel.type == "AGENCIA" || serviceChannel.type == "CAJERO") : serviceChannel.type == $serviceChannelTypeChecked.val() );
+               ( serviceChannelTypeCheckeds.length == 3 ? true : _.contains( _.pluck(serviceChannelTypeCheckeds, 'value'), serviceChannel.type) );
       }),
       template,
       results;
-  template = "<% _.each(serviceChannelsResults, function(serviceChannelResult) { %>" +
-             "<div class='app__block--results__layout__container__results__container__result app__block--results__layout__container__results__container__result--<%= serviceChannelResult.type %>'>" +
-             "  <header class='app__block--results__layout__container__results__container__result__header'>" +
-             "    <span><%= serviceChannelResult.type %></span> <%= serviceChannelResult.name %>" +
-             "  </header>" +
-             "  <p class='app__block--results__layout__container__results__container__result__data'><%= serviceChannelResult.address %></p>" +   
-             "  <p class='app__block--results__layout__container__results__container__result__data'><%= serviceChannelResult.phones %></p>" +
-             "  <p class='app__block--results__layout__container__results__container__result__data'><%= serviceChannelResult.customerSchedule %></p>" +
-             "  <div class='hidden invisible ir'>" +
-             "    <input type='hidden' value='<%= serviceChannelResult.latitude %>' class='latitude'>" +
-             "    <input type='hidden' value='<%= serviceChannelResult.longitude %>' class='longitude'>" +
-             "  </div>" +
-             "</div>" +
-             "<% }); %>";
+  template = '<% _.each(serviceChannelsResults, function(serviceChannelResult) { %>' +
+             '<div class="app__block--results__layout__container__results__container__result app__block--results__layout__container__results__container__result--<%= serviceChannelResult.type %>">' +
+             '  <header class="app__block--results__layout__container__results__container__result__header">' +
+             '    <span><%= serviceChannelResult.type %></span> <%= serviceChannelResult.name %>' +
+             '  </header>' +
+             '  <p class="app__block--results__layout__container__results__container__result__data"><%= serviceChannelResult.address %></p>' +   
+             '  <p class="app__block--results__layout__container__results__container__result__data"><%= serviceChannelResult.phones %></p>' +
+             '  <div class="hidden invisible ir">' +
+             '    <input type="hidden" value="<%= serviceChannelResult.latitude %>" class="latitude">' +
+             '    <input type="hidden" value="<%= serviceChannelResult.longitude %>" class="longitude">' +
+             '  </div>' +
+             '</div>' +
+             '<% }); %>';
   $('.app__block--results__layout__container__results__container').html( _.template(template, {serviceChannelsResults: serviceChannelsResults}) );
 }
 
